e26a1e3c7b586664ab177952622c5d7a9a50836f
GevorHovh <123312885+GevorHovh@users.noreply.github.com>
2023-01-29 13:17:26 +0400

    Add files via upload

---
+++ react.js
@@ -0,0 +1,22 @@
+
+let first = +prompt('Please number');
+let gorcoxutyun = prompt("gorcoxutyun");
+let tuo = +prompt("Please number");
+
+switch(gorcoxutyun) {
+    case "+":
+        console.log(first + tuo);
+        break;
+    case "-":
+        console.log(first - tuo);
+        break;
+    case "/" :
+        console.log(first / tuo);
+        break;
+    case "*":
+        console.log(first * tuo);
+    default:
+        
+        
+}
+
